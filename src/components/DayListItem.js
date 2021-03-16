8f69987e69c39f3b9bff5a42b1b28c6058c2c596
LB <lrbutler@gmail.com>
2021-03-15 17:39:23 -0700

    Fix spots remain logic

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,18 +1,30 @@
 import React from "react";
-import classnames from "classnames"
+import classnames from "classnames";
 import "components/DayListItem.scss";
-
 
 export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
-"day-list__item--selected": props.selected,
-"day-list__item--full": !props.spots,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
   });
+
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return "No spots remain";
+    }
+
+    if (spots === 1) {
+      return "There is one spot left";
+    }
+    if (spots > 1) {
+      return `There are ${spots} spots left`;
+    }
+  };
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
